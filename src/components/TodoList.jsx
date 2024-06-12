5557f3ea5d499911f95d45d86ce80fb22da81e71
kaustubhgandhi10 <kaustubh.gandhi10@gmail.com>
2024-06-12 12:05:25 +0530

    upload project

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,9 +2,12 @@ import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
+  const sortTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((items) => (
+      {sortTodos.map((items) => (
         <TodoItem
           key={items.name}
           items={items}
